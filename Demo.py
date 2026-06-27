41c0559765faeb006bb4b2edd9e4d84a75103f5e
KhuongTuanSon <sonkhuongtuan@gmail.com>
2026-06-27 09:01:57 +0700

    test_model

--- Demo.py
+++ Demo.py
@@ -10,9 +10,10 @@ from collections import deque, Counter
 # =============================================================================
 
 # Đường dẫn 
-MODEL_PATH = "E:/Smart_Crib/yolov26_250e_1000pic_702010/train/weights/best.pt" # Đường dẫn file mô hình
+MODEL_PATH = "D:/NCKH/Github/Smart_Crib/best.pt" # Đường dẫn file mô hình
 #MODEL_PATH = "E:/Smart_Crib/yolov26_250e_1000pic_702010/train/weights/best_ncnn_model"
-VIDEO_PATH = "E:/Smart_Crib/test/1100107673-preview.mp4" # Đường dẫn video test
+VIDEO_PATH = "D:/NCKH/Github/Smart_Crib/test_sleep.mp4" # Đường dẫn video test
+
 #VIDEO_PATH = 0
 # YOLO Class IDs 
 CLS_FACE = 1
